cdef2c0736af3bb0f4eb1d809c4a6cd578c51a53
Andrew <ramettaandrewj@gmail.com>
2021-02-14 10:22:23 -0500

    added seed data

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,5 @@
+INSERT INTO bookmarks (title, url, rating, description)
+VALUES
+('ESPN', 'https://www.espn.com', '5', 'Serving Sports Fans. Anytime. Anywhere.')
+('CNN', 'https://www.cnn.com', '4', 'Breaking News, Latest News and Videos.')
+('Youtube', 'https://www.youtube.com', '5', 'Youtube')
